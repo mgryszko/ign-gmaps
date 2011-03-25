7d5305cf1c777d3e20b71add513d2e7331cb45d5
Marcin Gryszko <marcin@gryszko.net>
2011-03-25 14:49:59 +0100

    Common fixtures for Utm and LatLng specs moved to a separate file

---
+++ test/util/spec-helpers.js
@@ -0,0 +1,25 @@
+ign.spec = {}
+
+ign.spec.pointsWithinSpainUtmZones = [
+    {latLng: new ign.LatLng(39.0, -24.0), utm: new ign.Utm(240199.81, 4321059.12, 27)},
+    {latLng: new ign.LatLng(40.0, -17.0), utm: new ign.Utm(329274.51, 4429672.97, 28)},
+    {latLng: new ign.LatLng(41.0, -10.0), utm: new ign.Utm(415897.87, 4539238.59, 29)},
+    {latLng: new ign.LatLng(42.0, -3.0), utm: new ign.Utm(500000, 4649776.22, 30)},
+    {latLng: new ign.LatLng(43.0, 4.0), utm: new ign.Utm(581508.65, 4761299.93, 31)}
+]
+
+ign.spec.pointsOnSpainBounds30NZoneLimit = [
+    {latLng: new ign.LatLng(44.0, -6.0), utm: new ign.Utm(259473.68, 4876249.13, 30)},
+    {latLng: new ign.LatLng(44.0, 0.0), utm: new ign.Utm(740526.32, 4876249.13, 30)},
+    {latLng: new ign.LatLng(35.0, 0.0), utm: new ign.Utm(773798.10, 3877156.69, 30)},
+    {latLng: new ign.LatLng(35.0, -6.0), utm: new ign.Utm(226201.90, 3877156.69, 30)}
+]
+
+ign.spec.pointsOnSpainBoundsOutsize30NZone = [
+    {latLng: new ign.LatLng(44.0, -7.0), utm: new ign.Utm(179294.18, 4879655.84, 30)},
+    {latLng: new ign.LatLng(44.0, 1.0), utm: new ign.Utm(820705.82, 4879655.84, 30)},
+    {latLng: new ign.LatLng(35.0, 1.0), utm: new ign.Utm(865108.81, 3880360.15, 30)},
+    {latLng: new ign.LatLng(35.0, -7.0), utm: new ign.Utm(134891.19, 3880360.15, 30)}
+]
+
+ign.spec.pointsOnSpainBounds = ign.spec.pointsOnSpainBounds30NZoneLimit.concat(ign.spec.pointsOnSpainBoundsOutsize30NZone)
